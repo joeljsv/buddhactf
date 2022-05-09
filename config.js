ebf9bac2abb7d3640b858276c913594c11a9314e
joeljsv <joelsunny495@gmail.com>
2022-05-09 11:55:20 +0530

    Added

---
+++ config.js
@@ -0,0 +1,13 @@
+const dbAuthUrl = "mongodb+srv://ctf:Gi8bN5uOVWEhZLnK@cluster0.wl2hu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //TODO
+const jwtSecretKey = "buddha"; //TODO;
+const registrationSpreadsheetId = "1UxCkMulF5W1UlnXh5Q_O7Z3Hc3f8dA8G4WzEjhnvEGo"; //TODO
+const questionSpreadSheetId = "1JVg73W5vq-SpJ80kTh2jXzo4ZkaGAlmlCAekJx-u1Nw"; //TODO
+
+module.exports = {
+  dbUrl: dbAuthUrl,
+  jwtSecretKey,
+  registrationSpreadsheetId,
+  questionSpreadSheetId,
+};
+
+
